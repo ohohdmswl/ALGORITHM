b78f2ff8e287f4ddf56e89453a31bb795cc83b70
이알지 <132552661+ohohdmswl@users.noreply.github.com>
2024-05-14 11:15:22 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int total = 0;
+		
+		for(int i = 1; i<=X ; i++) {
+			total += i;
+		}
+		System.out.println(total);
+	}//main
+}
